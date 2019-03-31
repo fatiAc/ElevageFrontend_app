16c3b1427f9689f81e85da339e8b50ac0aca2c3d
fatiAc <fatimaac391@gmail.com>
2019-03-31 11:45:08 +0200

    init project + Paddock examination

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,18 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
+import {Component} from '@angular/core';
+import {Platform} from 'ionic-angular';
+import {StatusBar} from '@ionic-native/status-bar';
+import {SplashScreen} from '@ionic-native/splash-screen';
 
-import { HomePage } from '../pages/home/home';
+import {AlimentationPage} from "../pages/alimentation/alimentation";
+import {SignInPage} from "../pages/sign-in/sign-in";
+import {DetailAlimentationPage} from "../pages/detail-alimentation/detail-alimentation";
+
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
+
+  rootPage: any = SignInPage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,60 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { StatusBar } from '@ionic-native/status-bar';
+import {BrowserModule} from '@angular/platform-browser';
+import {ErrorHandler, NgModule, CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
+import {IonicApp, IonicErrorHandler, IonicModule} from 'ionic-angular';
+import {SplashScreen} from '@ionic-native/splash-screen';
+import {StatusBar} from '@ionic-native/status-bar';
 
-import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+import {MyApp} from './app.component';
+import {HomePage} from '../pages/home/home';
+import {SignInPage} from "../pages/sign-in/sign-in";
+import {UserProvider} from '../providers/userProvider';
+import {HttpClientModule} from "@angular/common/http";
+import {HttpModule} from "@angular/http";
+import {DetailAlimentationPage} from "../pages/detail-alimentation/detail-alimentation";
+import {AlimentationPage} from "../pages/alimentation/alimentation";
+import {RecupAlimentationPage} from "../pages/recup-alimentation/recup-alimentation";
+import {DetailAlimentationProvider} from '../providers/detail-alimentation';
+import {NgxDatatableModule} from '@swimlane/ngx-datatable';
+import {PeriodeAlimentationProvider} from '../providers/periode-alimentation';
+import {NourritureProvider} from '../providers/nourritureProvider';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    SignInPage,
+    DetailAlimentationPage,
+    AlimentationPage,
+    RecupAlimentationPage
   ],
   imports: [
+    //added by me
+    HttpModule,
+    HttpClientModule,
+    NgxDatatableModule,
+    ///////
     BrowserModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    SignInPage,
+    DetailAlimentationPage,
+    AlimentationPage,
+    RecupAlimentationPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
-  ]
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    UserProvider,
+    DetailAlimentationProvider,
+    PeriodeAlimentationProvider,
+    NourritureProvider
+  ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
-export class AppModule {}
+export class AppModule {
+}
